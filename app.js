e5a8e7a5c9cd99a145e1031dc3d08ab1d9716c94
Jesus Gallardo <jjgalardior@gmail.com>
2020-07-22 07:31:03 -0430

    secont commit

--- app.js
+++ app.js
@@ -19,40 +19,68 @@ class UI
   {
     const productList = document.getElementById('product-list')
     const element = document.createElement('div')
+    element.classList.add('.card')
+    element.classList.add('.mb-4')
     element.innerHTML = `
-    <div class="card text-left mb-4">
-      <div class="card-body row">
-        <i class ="col mt-auto mb-auto">${product.name}</i> 
-        <i class ="col mt-auto mb-auto">${product.price}</i>
-        <i class ="col mt-auto mb-auto">${product.year}</i> 
-        <a class ="col btn mt-auto mb-auto far fa-trash-alt"></a> 
-      </div>
+    <div class="card-body row">
+      <i class ="col mt-auto mb-auto">${product.name}</i> 
+      <i class ="col mt-auto mb-auto">${product.price} $</i>
+      <i class ="col mt-auto mb-auto">${product.year}</i> 
+      <a class ="col btn mt-auto mb-auto fas fa-pen" name="edit"></a> 
+      <a class ="col btn mt-auto mb-auto fas fa-trash-alt" name="delete"></a> 
     </div>
     `
 
     productList.appendChild(element)
   }
 
-  deleteProduct() 
+  deleteProduct(product) 
   {
+    if (product.target.name === "delete" || product.target.name === "edit") {
+      let productToRemove = product.target.parentElement.parentElement
 
+      productToRemove.remove()
+    }
   }
 
-  editProduct() 
+  editProduct(product) 
   {
+    let productTarget = product.target.parentElement
+    let nameOld = productTarget.querySelectorAll(".mt-auto")[0].innerHTML
+    let priceOld = productTarget.querySelectorAll(".mt-auto")[1].innerHTML  
+    let yearOld = productTarget.querySelectorAll(".mt-auto")[2].innerHTML
+    
+    priceOld = parseInt(priceOld.slice(0, -1)) 
 
+    document.getElementById('name').value = nameOld
+    document.getElementById('price').value = priceOld
+    document.getElementById('year').value = yearOld
   }
   
-  ShowMessage() 
+  showMessage(message, classList) 
   {
+    const div = document.createElement('div')
+    div.className = `alert alert-${classList}`
+    div.appendChild(document.createTextNode(message))
+    
+    //Motrarndo en pantalla
 
+    const appContainer = document.querySelector('.appContainer')
+    const app = document.querySelector('#App')
+    appContainer.insertBefore(div, app)
+
+    setTimeout(() => {
+      div.remove()
+    }, 2500);
   }
 }
 
 // DOOM EVENTS
 
 let form = document.getElementById('products-form')
-
+let productList = document.getElementById('product-list')
+  
+  // AGREGAR PRODUCTO
 form.addEventListener("submit", e => {
   e.preventDefault()
   
@@ -63,6 +91,35 @@ form.addEventListener("submit", e => {
   let product = new Product(name,price,year)
   const ui = new UI
 
-  ui.addProduct(product)
-  ui.resetForm(form)
+  if( name === '' || price === '' || year === '')
+  {
+    ui.showMessage('Completa el formulario', 'info')
+  }else
+  {
+    ui.addProduct(product)
+    ui.resetForm(form)
+    ui.showMessage('Producto agregado', 'success')
+  }
+
+})
+  //ELIMINAR PRODUCTO
+productList.addEventListener("click", e => {
+  const ui = new UI
+  ui.deleteProduct(e)
+
+  if(e.target.name === 'delete') ui.showMessage('Producto eliminado', 'danger')
+})
+
+  //EDITAR PRODUCTO
+productList.addEventListener("click", e => {
+
+  const ui = new UI
+
+  if(e.target.name === "edit")
+  {
+    ui.editProduct(e)
+    ui.deleteProduct(e)
+    ui.showMessage('Edita a tu gusto en el formulario y vuelve a guardar', 'info')
+  }
+      
 })
